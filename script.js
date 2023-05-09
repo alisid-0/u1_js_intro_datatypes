0d3992a5393cabb9b81d919c3254c1ceb1646f93
Ali Siddiqui <alisiddiquia22@yahoo.com>
2023-05-09 10:43:57 -0500

    created and updated script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+//console.log(5+10)
+//console.log(5-10)
+//console.log(5/10)
+//console.log(5*10)
+
+//math operators + - / * 
+//exponents **
+//modulus - the remainder of a division equation
+
+//console.log(
+//    Math.pow(2.31,3.31), Math.sqrt(64), Math.random()*100
+  //  )333
+
+let firstName = "ali"
+let lastName = "sid"
+let fullName = `${firstName} ${lastName}`
+let age = 21
+console.log(`hwllo ${firstName} ${lastName}`)
+console.log(`hello ${fullName}!`)
